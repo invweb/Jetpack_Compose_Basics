f64716f857b43ef4db90a73f3c8dab9fd19b15be
Vasilii Karpenko <invweb@gmail.com>
2021-10-22 13:38:00 +0300

    Add files via upload

--- src/main/java/com/test/compose/MainActivity.kt
+++ src/main/java/com/test/compose/MainActivity.kt
@@ -3,18 +3,16 @@ package com.test.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -30,10 +28,16 @@ class MainActivity : ComponentActivity() {
                 Surface(color = MaterialTheme.colors.background) {
                     val navController = rememberNavController()
 
-                    NavHost(navController = navController, startDestination = "navigate1") {
-                        composable("navigate1") { Navigate1(navController) }
-                        composable("navigate2") { Navigate2(navController) }
+                    NavHost(navController = navController, startDestination = "navigate2") {
+                        composable("navigate1") {
+                            Navigate1(navController)
+                        }
+                        composable("navigate2") {
+                            Navigate2(navController)
+                        }
                     }
+
+//                    defaultPreview(navController)
                 }
             }
         }
@@ -53,24 +57,27 @@ fun Navigate1(navController: NavController) {
                     navController.navigate("navigate2")
                   },
         modifier = Modifier
-            .padding(8.dp, 48.dp, 0.dp, 0.dp)
+            .padding(48.dp, 48.dp, 0.dp, 0.dp)
     ) {
-        Text(text = "Navigate 1")
+        Text(text = "Navigate 2")
     }
 }
 
 @Composable
 fun Navigate2(navController: NavController) {
     Button(
-        onClick = { navController.navigate("navigate1")},
+        onClick = {
+                    navController.navigate("navigate1")
+                  },
         modifier = Modifier
-            .padding(8.dp, 56.dp, 0.dp, 0.dp)
+            .padding(18.dp, 56.dp, 0.dp, 0.dp)
     ) {
-        Text(text = "Navigate 2")
+        Text(text = "Navigate 1")
     }
 
     Text(text = "Test",
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier
+            .fillMaxWidth()
             .padding(8.dp, 24.dp, 0.dp, 0.dp))
 
     Greeting("Android")
@@ -78,8 +85,28 @@ fun Navigate2(navController: NavController) {
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    ComposeTheme {
-        Greeting("Android")
+fun defaultPreview(navController: NavHostController) {
+    val materialBlue700= Color(0xFF1976D2)
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = {Text("TopAppBar")},backgroundColor = materialBlue700)  },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = { FloatingActionButton(onClick = {}){
+            Text("X")
+        } },
+        drawerContent = { Text(text = "drawerContent") },
+        content = { Text("BodyContent") },
+        bottomBar = { BottomAppBar(backgroundColor = materialBlue700) { Text("BottomAppBar") } }
+    )
+
+    Button(
+        onClick = {
+            navController.navigate("navigate2")
+        },
+        modifier = Modifier
+            .padding(108.dp, 48.dp, 0.dp, 0.dp)
+    ) {
+        Text(text = "Navigate 2")
     }
 }
